847b4ed8a30a551c36f99158bfa6208301dfe5a2
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-05-25 10:42:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿var menus = Directory.GetFiles(@"C:\Users\Kathi\Dokumente\c#-codes\POSE-Codes\059-schnitzelhunt\menucards");
+
+PrintWelcomingMessage();
+
+for (int i = 0; i < menus.Length; i++)
+{
+    string[] lines = File.ReadAllLines(menus[i]);
+    bool firstOccurence = true;
+
+    foreach (var line in lines)
+        if (line.Contains("Schnitzel"))
+        {
+            if (firstOccurence)
+            {
+                Console.WriteLine(Path.GetFileNameWithoutExtension(menus[i]));
+                firstOccurence = false;
+            }
+            int indexOfEnd = line.IndexOf(':');
+            Console.WriteLine($"    {line.Substring(0, indexOfEnd)}");
+        }
+}
+
+void PrintWelcomingMessage()
+{
+    Console.WriteLine("WHERE TO GET SCHNITZEL?");
+    Console.WriteLine("=======================");
+    Console.WriteLine();
+}
