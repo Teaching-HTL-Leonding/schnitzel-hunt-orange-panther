eacb366a83a0f2b85287b094c41e26f230a7b342
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-05-25 12:20:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,91 @@
-﻿var menus = Directory.GetFiles(@"C:\Users\Kathi\Dokumente\c#-codes\POSE-Codes\059-schnitzelhunt\menucards");
+﻿Console.OutputEncoding = System.Text.Encoding.Default;
+var menus = Directory.GetFiles(@"C:\Users\Kathi\Dokumente\c#-codes\POSE-Codes\059-schnitzelhunt\menucards");
 
-PrintWelcomingMessage();
+PrintAllRestaurantThatHaveSchnitzel(menus);
+Console.WriteLine();
+PrintSpecifiedDish(menus, "Seitan Schnitzel", "THE CHEAPEST");
+Console.WriteLine();
+PrintSpecifiedDish(menus, "Seitan Schnitzel", "THE MOST EXPENSIVE");
 
-for (int i = 0; i < menus.Length; i++)
-{
-    string[] lines = File.ReadAllLines(menus[i]);
-    bool firstOccurence = true;
-
-    foreach (var line in lines)
-        if (line.Contains("Schnitzel"))
-        {
-            if (firstOccurence)
-            {
-                Console.WriteLine(Path.GetFileNameWithoutExtension(menus[i]));
-                firstOccurence = false;
-            }
-            int indexOfEnd = line.IndexOf(':');
-            Console.WriteLine($"    {line.Substring(0, indexOfEnd)}");
-        }
-}
-
-void PrintWelcomingMessage()
+void PrintAllRestaurantThatHaveSchnitzel(string[] menus)
 {
     Console.WriteLine("WHERE TO GET SCHNITZEL?");
     Console.WriteLine("=======================");
     Console.WriteLine();
+
+    for (int i = 0; i < menus.Length; i++)
+    {
+        string[] lines = File.ReadAllLines(menus[i]);
+        bool firstOccurence = true;
+
+        foreach (var line in lines)
+            if (line.Contains("Schnitzel"))
+            {
+                if (firstOccurence)
+                {
+                    Console.WriteLine(Path.GetFileNameWithoutExtension(menus[i]));
+                    firstOccurence = false;
+                }
+                int indexOfEnd = line.IndexOf(':');
+                Console.WriteLine($"    {line.Substring(0, indexOfEnd)}");
+            }
+    }
 }
+
+void PrintSpecifiedDish(string[] menus, string searchedDish, string parameter)
+{
+    Console.WriteLine($"WHERE TO GET {parameter} {searchedDish.ToUpper()}?");
+    Console.WriteLine($"=========================================");
+    Console.WriteLine();
+
+    int count = 0, currentPrice = 0, endPrice = 0;
+    string restaurant = "";
+    for (int i = 0; i < menus.Length; i++)
+    {
+        string[] lines = File.ReadAllLines(menus[i]);
+
+        foreach (var line in lines)
+        {
+            if (line.Contains(searchedDish))
+            {
+                string price = "";
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (Char.IsDigit(line[j]))
+                    {
+                        price += line[j];
+                    }
+                }
+
+                if (count == 0)
+                {
+                    endPrice = int.Parse(price);
+                }
+                currentPrice = int.Parse(price);
+
+                if (parameter == "THE CHEAPEST")
+                {
+                    if (currentPrice < endPrice)
+                    {
+                        endPrice = currentPrice;
+                        restaurant = Path.GetFileNameWithoutExtension(menus[i]);
+                    }
+                }
+                else if (parameter == "THE MOST EXPENSIVE")
+                {
+                    if (currentPrice > endPrice)
+                    {
+                        endPrice = currentPrice;
+                        restaurant = Path.GetFileNameWithoutExtension(menus[i]);
+                    }
+                }
+                count++;
+            }
+        }
+    }
+    if (parameter == "THE CHEAPEST") { Console.WriteLine($"{restaurant}, {endPrice}€"); }
+    if (parameter == "THE MOST EXPENSIVE") { Console.WriteLine($"{restaurant}, {endPrice}"); }
+
+}
+
+
